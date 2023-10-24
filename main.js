5be5a947a3d58b1c26776956575b35cfc46d361c
simonwaechter1 <waechthor94@gmail.com>
2023-10-24 22:48:00 +0200

    compiled with flag -t "es2015"

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 function printFirstNames(friends) {
-    for (var _i = 0, friends_1 = friends; _i < friends_1.length; _i++) {
-        var friend = friends_1[_i];
+    for (let friend of friends) {
         console.log(friend.firstName);
     }
 }
